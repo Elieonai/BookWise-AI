645796029c619d057864737abd606906003f338c
Vitor Camilo <vittor.camilo@gmail.com>
2026-06-23 16:21:34 -0300

    feat: add bookRoutes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bookRoutes = require('./routes/bookRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api', bookRoutes);
 app.use('/api', reviewRoutes);
 
 module.exports = app;

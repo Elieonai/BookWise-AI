2cd7e3bcdddbc065b768b520b16cab1e543678b1
Pedro Silva <phps.ti2000@gmail.com>
2026-06-23 21:00:18 -0300

    adição do cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const bookRoutes = require('./routes/bookRoutes');
-const reviewRoutes = require('./routes/reviewRoutes');
+const express = require("express");
+const cors = require("cors");
+const bookRoutes = require("./routes/bookRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
-app.use('/api', bookRoutes);
-app.use('/api', reviewRoutes);
+app.use("/api", bookRoutes);
+app.use("/api", reviewRoutes);
 
 module.exports = app;
